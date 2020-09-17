9283c7d7e477c9291d56a5148bea468a617d67c6
박훈 <hundl5789@nate.com>
2020-09-17 18:02:34 +0900

    #ListBox 삭제 + SMS 정보 추가

---
+++ ServerMonitoring/Models/SMSInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerMonitoring.Models
+{
+    class SMSInfo
+    {
+        private string _id;
+        private string _pw;
+        private string _to;
+        private string _msg;
+
+        public string Id { get => _id; set => _id = value; }
+        public string Pw { get => _pw; set => _pw = value; }
+
+
+    }
+}
